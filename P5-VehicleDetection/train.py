1fc49d8e88a64b18993a7a26ae897e7f02692338
Yifei Li <workingyifei@gmail.com>
2017-02-09 23:05:45 -0800

    picked best model

--- P5-VehicleDetection/train.py
+++ P5-VehicleDetection/train.py
@@ -70,14 +70,14 @@ y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
 
 # Split up data into randomized training and test sets
 rand_state = np.random.randint(0, 100)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rand_state)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 print('Feature vector length:', len(X_train[0]))
 
   
 
 # Train a classifier with created data. 
 # Use a linear SVC 
-svc = LinearSVC()
+svc = LinearSVC(loss='hinge')
 
 # Check the training time for the SVC
 t=time.time()
